a976584d888e6bb060002125e1bbb017ed431046
Prathamesh Kakade <itsmepsk@gmail.com>
2015-01-25 00:15:11 +0530

    added more fields to details view

--- application/views/details.php
+++ application/views/details.php
@@ -27,8 +27,8 @@
 			
 			$f_name					=			array(
 				'type'				=>				'text',
-				'name'				=>				'registration_no',
-				'id'				=>				'registration_no',
+				'name'				=>				'name',
+				'id'				=>				'name',
 				'class'				=>				'',
 				'aria-requires'		=>				'required',
 				'value'				=>				''
@@ -41,9 +41,31 @@
 				'cse'				=>				'Computer Science and Engineering',
 				'ee'				=>				'Electrical Engineering',
 				'ece'				=>				'Electronics and Communication',
-				''
+				'it'				=>				'Information Technology',
+				'me'				=>				'Mechanical Engineering',
+				'mme'				=>				'Metallurgical & Materials Engineering'
 			);
 			
+			$f_sex					=			array(
+				'male'				=>				"Male",
+				'female'			=>				"Female"
+			);
+			
+			$f_date					=			array(
+				'type'				=>				'text',
+				'name'				=>				'date',
+				'id'				=>				'date',
+				'class'				=>				'',
+				'aria-requires'		=>				'required',
+				'value'				=>				''
+			);
+			
+			$f_category				=			array(
+				'gen'				=>				'General',
+				'obc'				=>				'OBC',
+				'sc'				=>				'SC',
+				'st'				=>				'ST'
+			);
 		?>
 	
 		<?php echo form_open(base_url().'details/submit'); ?>
@@ -57,7 +79,18 @@
 		<?php echo form_input($f_name); ?>
 		<br>
 		<label>Branch:</label>
-		<?php echo form_dropdown("branch",$f_branch); ?>
+		<?php echo form_dropdown("branch",$f_branch,"ece"); ?>
+		<br>
+		<label>Sex:</label>
+		<?php echo form_dropdown("sex", $f_sex); ?>
+		<br>
+		<label>Date of Birth:</label>
+		<?php echo form_input($f_date); ?>
+		<br>
+		<label>Category:</label>
+		<?php echo form_dropdown("category",$f_category); ?>
+		<br>
+		<label>Email:</label>
 		<?php echo form_close(); ?>
 	</body>
 </html>
